c457510f2e0d027908ab3dfb8558fa681d87fe1d
Sergii Kostanets <sergiikostanets@gmail.com>
2023-04-21 14:29:34 +0100

    setup development environment

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -17,6 +17,8 @@ import dj_database_url
 if os.path.exists('env.py'):
     import env
 
+development = os.environ.get('DEVELOPMENT', False)
+
 # SECRET_KEY_ENV = os.getenv('SECRET_KEY_ENV')
 SECRET_KEY_ENV = os.environ.get('SECRET_KEY_ENV')
 
@@ -31,12 +33,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = SECRET_KEY_ENV
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = development
 
-# ALLOWED_HOSTS = ['codeinstitute-todo.herokuapp.com']
-ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME'), '127.0.0.1']
-
-
+if development:
+    ALLOWED_HOSTS = ['127.0.0.1']
+else:
+    ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 # Application definition
 
@@ -84,17 +86,17 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-}
+if development:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
 
 
 # Password validation
